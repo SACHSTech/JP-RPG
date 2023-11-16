a5169ee6402616fea24b2d94e1a1b788e0e3f827
Jacky-Ho0429 <jacky.ho25@ycdsbk12.ca>
2023-11-16 17:39:24 -0500

    Basic Movement

--- Sketch.java
+++ Sketch.java
@@ -1,16 +1,99 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
+    PImage imgCharacter_Up;
+    PImage imgCharacter_Down;
+    PImage imgCharacter_Left;
+    PImage imgCharacter_Right;
+
+    float fltPlayerX = 112;
+    float fltPlayerY = 112;
+
+    String strDirection = "down";
+
+    boolean keyW, keyS, keyA, keyD;
 
     public void settings() {
-        size(600, 600);
+        size(240, 240);
     }
 
     public void setup() {
-
+        imgCharacter_Up = loadImage("Images/Player/Character_Up.png");
+        imgCharacter_Down = loadImage("Images/Player/Character_Down.png");
+        imgCharacter_Left = loadImage("Images/Player/Character_Left.png");
+        imgCharacter_Right = loadImage("Images/Player/Character_Right.png");
     }
 
     public void draw() {
+        background(255);
 
+        movePlayer();
+        checkWallCollision();
+        displayPlayer();
+    }
+
+    private void movePlayer() {
+        if (keyW) {
+            fltPlayerY -= 16;
+            keyW = false;
+            strDirection = "up";
+        } else if (keyS) {
+            fltPlayerY += 16;
+            keyS = false;
+            strDirection = "down";
+        } else if (keyA) {
+            fltPlayerX -= 16;
+            keyA = false;
+            strDirection = "left";
+        } else if (keyD) {
+            fltPlayerX += 16;
+            keyD = false;
+            strDirection = "right";
+        }
+    }
+
+    private void checkWallCollision() {
+        fltPlayerX = constrain(fltPlayerX, 0, width - 16);
+        fltPlayerY = constrain(fltPlayerY, 0, height - 16);
+    }
+
+    private void displayPlayer() {
+        if (strDirection.equals("up")) {
+
+            image(imgCharacter_Up, fltPlayerX, fltPlayerY);
+
+        } else if (strDirection.equals("down")) {
+
+            image(imgCharacter_Down, fltPlayerX, fltPlayerY);
+
+        } else if (strDirection.equals("left")) {
+
+            image(imgCharacter_Left, fltPlayerX, fltPlayerY);
+
+        } else if (strDirection.equals("right")) {
+
+            image(imgCharacter_Right, fltPlayerX, fltPlayerY);
+
+        }
+    }
+
+    public void keyPressed() {
+        if (key == 'W' || key == 'w') {
+
+            keyW = true;
+
+        } else if (key == 'S' || key == 's') {
+
+            keyS = true;
+
+        } else if (key == 'A' || key == 'a') {
+
+            keyA = true;
+
+        } else if (key == 'D' || key == 'd') {
+
+            keyD = true;
+        }
     }
 }
