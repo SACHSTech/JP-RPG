c324749ea2c8dbb99168a840c7520b339e6cb9d1
Preston-Wong <you@example.com>
2023-11-24 15:19:39 -0500

    Added background music

--- Main.java
+++ Main.java
@@ -1,18 +1,35 @@
 import processing.core.PApplet;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.sound.sampled.*;
 
 /**
  * Main class to execute sketch
- * @author 
+ * @author Preston Wong and Jacky Ho
  *
  */
 class Main {
-  public static void main(String[] args) {
-    
+  public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
     String[] processingArgs = {"MySketch"};
 	  Sketch mySketch = new Sketch();
-    PlayMusic myMusic = new PlayMusic();
 	  PApplet.runSketch(processingArgs, mySketch);
-   
+
+  // plays background music
+  Scanner scanner = new Scanner(System.in);
+		
+	File file = new File("playmusic/Music2.wav");
+	AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+	Clip clip = AudioSystem.getClip();
+	clip.open(audioStream);
+	
+	clip.start();
+	
+	String response = scanner.next(); 
+    
+
+
   }
   
 }
