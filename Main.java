39f44ad526153d15287a00362d168dede6241668
Preston-Wong <you@example.com>
2023-11-24 15:52:03 -0500

    music loops on forever

--- Main.java
+++ Main.java
@@ -13,23 +13,29 @@ class Main {
   public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 
     String[] processingArgs = {"MySketch"};
-	  Sketch mySketch = new Sketch();
-	  PApplet.runSketch(processingArgs, mySketch);
+	Sketch mySketch = new Sketch();
+	PApplet.runSketch(processingArgs, mySketch);
 
-  // plays background music
-  Scanner scanner = new Scanner(System.in);
+
+    // plays background music
+    Scanner scanner = new Scanner(System.in);
 		
+	// finds the wav file to play
 	File file = new File("playmusic/Music2.wav");
 	AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+
+	// preps the music file, opens it, and plays it from the start and makes go forever
 	Clip clip = AudioSystem.getClip();
 	clip.open(audioStream);
-	
+	clip.loop(Clip.LOOP_CONTINUOUSLY);
 	clip.start();
-	
+
 	String response = scanner.next(); 
+	
+
+	}
     
 
 
   }
   
-}
